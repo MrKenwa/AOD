2483b9aa420bbc3b66e428b7eabd23226a928b8d
Maxim <86079870+MrKenwa@users.noreply.github.com>
2021-08-30 23:45:59 +0300

    Fin

--- js/script.js
+++ js/script.js
@@ -40,3 +40,27 @@ $(document).ready(function(){
 
 //</Slider>==============================================
 
+//<Прокрутка при клике>==============================================
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+   menuLinks.forEach(menuLink => {
+      menuLink.addEventListener("click", onMenuLinkClick);
+   });
+
+   function onMenuLinkClick(e) {
+      const menuLink = e.target;
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+         const gotoBlock = document.querySelector(menuLink.dataset.goto);
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+         window.scrollTo ({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+         e.preventDefault();
+      }
+   }
+}
+
+//</Прокрутка при клике>==============================================
